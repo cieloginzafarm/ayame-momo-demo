99446169d9f19baaf91d2855df26be354ad89ff5
cielojordanjp <cielo@ginzafarm.co.jp>
2020-09-07 16:40:18 +0900

    Fixed video codec bug

--- pages/videos/_roomId/index.vue
+++ pages/videos/_roomId/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <br />
     <h1>{{ video.name }}</h1>
     <br />
     <p>ルームID: {{ video.roomId }}</p>
@@ -7,8 +8,12 @@
     <p>クライアントID: {{ conn.options.clientId }}</p>
     <br />
     <p>シグナリングキー: {{ signalingKey }}</p>
-    <br /><button class="button--grey" @click="connect">Connect</button>
-    <button class="button--grey" @click="disconnect">Disconnect</button>
+    <br />
+    <div>
+      <button class="button--grey" @click="connect">Connect</button>
+      <button class="button--grey" @click="disconnect">Disconnect</button>
+    </div>
+    <br />
     <p>
       映像コーデック
       <select v-model="videoCodec" placeholder="videoCodec">
@@ -19,6 +24,7 @@
       </select>
       （Google Chrome、Microsoft Edge でのみで動作します）
     </p>
+    <br />
     <div>
       <div style="float:left;">
         <video
@@ -29,10 +35,6 @@
           controls
           style="width: 400px; height: 300px; border: 1px solid black;"
         ></video>
-      </div>
-      <br />
-      <div>
-        <pre>{{ this.conn }} </pre>
       </div>
     </div>
   </div>
@@ -48,7 +50,11 @@ export default {
   props: ["video"],
   data() {
     return {
-      conn: AyameConnection("wss://ayame-lite.shiguredo.jp/signaling", this.video.roomId, this.defaultOptions),
+      conn: AyameConnection(
+        "wss://ayame-lite.shiguredo.jp/signaling",
+        this.video.roomId,
+        this.defaultOptions
+      ),
       mediaStream: null,
       signalingKey: "jPnJsAZWKSIro0toyuktmbuAJkzn6636TwibJKDXjN9OOjMU",
       videoCodec: "none"
@@ -56,36 +62,54 @@ export default {
   },
   methods: {
     async connect() {
-      if (this.signalingKey) {
-        this.conn.options.signalingKey = this.signalingKey;
-      }
-      this.conn.options.video.direction = "recvonly";
-      this.conn.options.audio.direction = "recvonly";
-      this.conn.options.video.codec = this.videoCodec;
-      if (this.videoCodec == "none") {
-        this.conn.options.video.codec = null;
-      }
-      try {
-        this.conn.connect(this.mediaStream);
-        this.conn.on("open", ({ authzMetadata }) => console.log(authzMetadata));
-        this.conn.on("disconnect", e => console.log(e));
-        this.conn.on("addstream", async e => {
-          await console.log(e.stream);
-          this.mediaStream = e.stream;
-        });
-      } catch (error) {
-        console.log("ERROR: ", error);
+      // Check if connection already exists
+      if ((this.conn._ws == null) | (this.conn._pc == null)) {
+        if (this.signalingKey) {
+          this.conn.options.signalingKey = this.signalingKey;
+        }
+        this.conn.options.video.direction = "recvonly";
+        this.conn.options.audio.direction = "recvonly";
+        this.conn.options.video.codec = this.videoCodec;
+        if (this.videoCodec == "none") {
+          this.conn.options.video.codec = null;
+        }
+        try {
+          this.conn.connect(this.mediaStream);
+          console.log(this.conn);
+          console.log("Connection initialized.");
+          this.conn.on("open", ({ authzMetadata }) =>
+            console.log(authzMetadata)
+          );
+          this.conn.on("disconnect", e => console.log(e));
+          this.conn.on("addstream", async e => {
+            await console.log(e.stream);
+            this.mediaStream = e.stream;
+          });
+        } catch (error) {
+          console.log("ERROR: ", error);
+        }
       }
     },
     disconnect() {
-      if (this.conn) {
+      // If connection exists, disconnect
+      if ((this.conn._ws != null) | (this.conn._pc != null)) {
         this.conn.disconnect();
-        console.log("Disconnected");
+        console.log("Connection disconnected.");
       }
+    },
+    sleep(milliseconds) {
+      return new Promise(resolve => setTimeout(resolve, milliseconds));
     }
   },
+  watch: {
+    videoCodec: function(value) {
+      this.conn.options.video.codec = value;
+      console.log(this.conn.options.video.codec);
+    }
+  },
+
   head: {
-    title: "Stream"
+    title: "Ayame-Momo WebRTC ビデオ"
   }
 };
 </script>
